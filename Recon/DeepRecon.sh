3e1c84977c476a3f7c3e763bd8a9c86bce3ba10a
robin113x <robin113x@gmail.com>
2025-02-26 18:20:25 +0530

    Auto-commit: 2025-02-26 18:20:25

--- Recon/DeepRecon.sh
+++ Recon/DeepRecon.sh
@@ -15,7 +15,7 @@ echo "********************************Passive Enumeration***********************
 curl -s "https://crt.sh/?q=%.$domain&output=json" | jq -r '.[].name_value' | sed 's/\*\.//g' | sort -u | tee "$output/crtsh.txt"
 curl -s "https://otx.alienvault.com/api/v1/indicators/hostname/$domain/passive_dns" | jq -r '.passive_dns[]?.hostname' | sort -u | tee "alienvault.txt"
 curl -s "https://urlscan.io/api/v1/search/?q=domain:$domain&size=10000" | jq -r '.results[]?.page?.domain' | sort -u | tee "$output/urlscan.txt"
-curl -s "http://web.archive.org/cdx/search/cdx?url=*.${domain}/*&output=json&collapse=urlkey" | jq -r '.[1:][] | .[2]' | sort -u | tee "$output/wayback.txt"
+curl -s "http://web.archive.org/cdx/search/cdx?url=*.${domain}/*&output=json&collapse=urlkey" | jq -r '.[1:][] | .[2]' | sort -u | tee "wayback.txt"
 
 
 echo "********************************Active Enumeration*********************************************"
