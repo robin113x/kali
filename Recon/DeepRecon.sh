a0f1b8e192c69b2c15ad133333e71128d9c1f429
robin113x <robin113x@gmail.com>
2025-02-26 18:32:08 +0530

    Auto-commit: 2025-02-26 18:32:08

--- Recon/DeepRecon.sh
+++ Recon/DeepRecon.sh
@@ -14,6 +14,7 @@ pwd
 sleep 5
 
 
+
 echo "********************************Passive Enumeration*********************************************"
 curl -s "https://crt.sh/?q=%.$domain&output=json" | jq -r '.[].name_value' | sed 's/\*\.//g' | sort -u | tee "crtsh_passive.txt"
 curl -s "https://otx.alienvault.com/api/v1/indicators/hostname/$domain/passive_dns" | jq -r '.passive_dns[]?.hostname' | sort -u | tee "alienvault_passive.txt"
