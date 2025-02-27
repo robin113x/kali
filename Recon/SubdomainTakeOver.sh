175be3a2f57bb6ed16c016b98976d2d8f2eb265a
robin113x <robin113x@gmail.com>
2025-02-28 01:16:27 +0530

    Auto-commit: 2025-02-28 01:16:27

--- Recon/SubdomainTakeOver.sh
+++ Recon/SubdomainTakeOver.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# Define the input file
+input_file="/path/to/your/input_file.txt"
+
 # Get Subdomains from TLS Certificates
 cero -p 443,4443,8443,10443 -c 1000
 
 # Get Subdomains From CNAMES
-dnsx -retry 3 -cname -l <input_file>
+dnsx -retry 3 -cname -l "$input_file"
+
 # Check For Subdomain Takeover
-subjack -w <input_file> -t 100 -timeout 30 -ssl -c /path/to/fingerprints.json -v 3
+subjack -w "$input_file" -t 100 -timeout 30 -ssl -c /path/to/fingerprints.json -v 3
