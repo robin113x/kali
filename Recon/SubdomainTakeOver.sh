e9cf499c87b9aa1b8d0b025e7751a0491ebbcd52
robin113x <robin113x@gmail.com>
2025-02-28 01:16:15 +0530

    Auto-commit: 2025-02-28 01:16:15

--- Recon/SubdomainTakeOver.sh
+++ Recon/SubdomainTakeOver.sh
@@ -5,3 +5,5 @@ cero -p 443,4443,8443,10443 -c 1000
 
 # Get Subdomains From CNAMES
 dnsx -retry 3 -cname -l <input_file>
+# Check For Subdomain Takeover
+subjack -w <input_file> -t 100 -timeout 30 -ssl -c /path/to/fingerprints.json -v 3
