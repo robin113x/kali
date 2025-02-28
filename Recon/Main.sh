6e4eb7ba1eee1ee95dc2826de24fcfd062f51a45
robin113x <robin113x@gmail.com>
2025-02-28 23:08:25 +0530

    Auto-commit: 2025-02-28 23:08:25

--- Recon/Main.sh
+++ Recon/Main.sh
@@ -6,7 +6,7 @@ echo "Domain: $DOMAIN"
 OUTPUT_DIR="results/$DOMAIN"
 LOG_FILE="recon.log"
 NUCLEI_TEMPLATES="/root/nuclei-templates
-SHODAN_API=$
+SHODAN_API=$SHODAN_API_KEY
 CHAOS_API="a11c9305-a0b8-42e7-b6ba-f92349513c66"
 CENSYS_API
 WORDLIST="/usr/share/wordlist/n0kovo_subdomains/n0kovo_subdomains_large.txt"
