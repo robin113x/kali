ac72c3a9f9d5a652493a0ed0cc4c5da72bd9756d
robin113x <robin113x@gmail.com>
2025-02-28 22:56:48 +0530

    Auto-commit: 2025-02-28 22:56:48

--- Recon/Main.sh
+++ Recon/Main.sh
@@ -10,4 +10,4 @@ SHODAN_API="22905caf0b00f7f97261d498140046a07925b8c22f8d21b12fcad6b1b8357105"
 CHAOS_API="a11c9305-a0b8-42e7-b6ba-f92349513c66"
 CENSYS_API
 WORDLIST="/usr/share/wordlist/n0kovo_subdomains/n0kovo_subdomains_large.txt"
-DNS_RESOLVER="resolvers.txt
+DNS_RESOLVER="/usr/share/wordlist/resolvers.txt"
