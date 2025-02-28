127156ab45b952453b0f799a41ad95ccdad8621f
robin113x <robin113x@gmail.com>
2025-02-28 22:49:40 +0530

    Auto-commit: 2025-02-28 22:49:40

--- Recon/Main.sh
+++ Recon/Main.sh
@@ -9,3 +9,5 @@ NUCLEI_TEMPLATES="/root/nuclei-templates
 SHODAN_API="22905caf0b00f7f97261d498140046a07925b8c22f8d21b12fcad6b1b8357105"
 CHAOS_API="a11c9305-a0b8-42e7-b6ba-f92349513c66"
 CENSYS_API
+WORDLIST="/usr/share/wordlists/subdomains-top1million-5000.txt"
+DNS_RESOLVER="
