00bd92716bb6336fa6f2cca16a5597bd7948de36
robin113x <robin113x@gmail.com>
2025-02-28 23:29:49 +0530

    Auto-commit: 2025-02-28 23:29:49

--- Recon/Subdomain/PassiveRecon.sh
+++ Recon/Subdomain/PassiveRecon.sh
@@ -3,9 +3,9 @@ passive_enum() {
     echo "******************************** Passive Enumeration ********************************"
 
     curl -s "https://crt.sh/?q=%.$domain&output=json" | jq -r '.[].name_value' | sed 's/\*\.//g' | sort -u | tee "$OUTPUT_DIR/crtsh_passive.txt"
-    curl -s "https://otx.alienvault.com/api/v1/indicators/hostname/$domain/passive_dns" | jq -r '.passive_dns[]?.hostname' | sort -u | tee "alienvault_passive.txt"
-    curl -s "https://urlscan.io/api/v1/search/?q=domain:$domain&size=10000" | jq -r '.results[]?.page?.domain' | sort -u | tee "urlscan_passive.txt"
-    curl -s "http://web.archive.org/cdx/search/cdx?url=*.${domain}/*&output=json&collapse=urlkey" | jq -r '.[1:][] | .[2]' | sort -u | tee "wayback_passive.txt"
+    curl -s "https://otx.alienvault.com/api/v1/indicators/hostname/$domain/passive_dns" | jq -r '.passive_dns[]?.hostname' | sort -u | tee "$OUTPUT_DIR/alienvault_passive.txt"
+    curl -s "https://urlscan.io/api/v1/search/?q=domain:$domain&size=10000" | jq -r '.results[]?.page?.domain' | sort -u | tee "$OUTPUT_DIR/urlscan_passive.txt"
+    curl -s "http://web.archive.org/cdx/search/cdx?url=*.${domain}/*&output=json&collapse=urlkey" | jq -r '.[1:][] | .[2]' | sort -u | tee "$OUTPUT_DIR/wayback_passive.txt"
     wait
 }
 
