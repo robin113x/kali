99c6dbc158a5197b6c09a2a41a393a223be0dcfd
robin113x <robin113x@gmail.com>
2025-02-28 22:43:33 +0530

    Auto-commit: 2025-02-28 22:43:33

--- Recon/Subdomain/ActiveRecon.sh
+++ Recon/Subdomain/ActiveRecon.sh
@@ -1,12 +1,12 @@
 # Active enumeration
 active_enum() {
     echo "******************************** Active Enumeration ********************************"
-    subfinder -d "$domain" -all -recursive -o "subfinder_subdomain.txt"
-    assetfinder --subs-only "$domain" | tee "assetfinder_subdomain.txt"
-    shodan search domain:"$domain" | jq -r '.matches[]?.hostnames[]' | sort -u | tee "shodan_subdomain.txt"
-    shodanx subdomain -d "$domain" -ra -o "shodanx_subdomain.txt"
-    findomain -t "$domain" -v -o findomain_subdomain.txt
-    amass enum -norecursive  -d "$domain" -o amass_subdomain.txt
+    subfinder -d "$DOMAIN" -all -recursive -o "subfinder_subdomain.txt"
+    assetfinder --subs-only "$DOMAIN" | tee "assetfinder_subdomain.txt"
+    shodan search domain:"$DOMAIN" | jq -r '.matches[]?.hostnames[]' | sort -u | tee "shodan_subdomain.txt"
+    shodanx subdomain -d "$DOMAIN" -ra -o "shodanx_subdomain.txt"
+    findomain -t "$DOMAIN" -v -o findomain_subdomain.txt
+    amass enum -norecursive  -d "$DOMAIN" -o amass_subdomain.txt
     anubis -d "$DOMAIN" -o "anubis_subs.txt" 
     chaos -d "$DOMAIN" -k "$CHAOS_KEY" -o "chaos_subs.txt" 
     sublist3r -d "$DOMAIN" -o "sublist3r_subs.txt" 
@@ -17,8 +17,8 @@ active_enum() {
 # Brute-force enumeration
 bruteforce_enum() {
     echo "******************************** Bruteforce Enumeration ********************************"
-    subrute -d "$domain" | tee "subbrute_subdomain.txt"
-    gobuster dns -d "$domain" -w /usr/share/wordlist/n0kovo_subdomains/n0kovo_subdomains_large.txt -o gobuster_subdomain.txt
+    subrute -d "$DOMAIN" | tee "subbrute_subdomain.txt"
+    gobuster dns -d "$DOMAIN" -w /usr/share/wordlist/n0kovo_subdomains/n0kovo_subdomains_large.txt -o gobuster_subdomain.txt
    
 }
 
