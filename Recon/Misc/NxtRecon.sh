84a7914a1583ef038efc4e4ec177156a856e7276
robin113x <robin113x@gmail.com>
2025-02-28 23:35:23 +0530

    Auto-commit: 2025-02-28 23:35:23

--- Recon/Misc/NxtRecon.sh
+++ Recon/Misc/NxtRecon.sh
@@ -30,7 +30,6 @@ passive_enum() {
 curl -s "https://otx.alienvault.com/api/v1/indicators/hostname/$domain/passive_dns" | jq -r '.passive_dns[]?.hostname' | sort -u | tee "$output/alienvault.txt"
 curl -s "https://urlscan.io/api/v1/search/?q=domain:$domain&size=10000" | jq -r '.results[]?.page?.domain' | sort -u | tee "$output/urlscan.txt"
 curl -s "http://web.archive.org/cdx/search/cdx?url=*.${domain}/*&output=json&collapse=urlkey" | jq -r '.[1:][] | .[2]' | sort -u | tee "$output/wayback.txt"
-}
 
 # Active enumeration
 active_enum() {
