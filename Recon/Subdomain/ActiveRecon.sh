93f776c86ae563b068751aad387a9fc809dc1cb4
robin113x <robin113x@gmail.com>
2025-02-28 22:42:40 +0530

    Auto-commit: 2025-02-28 22:42:40

--- Recon/Subdomain/ActiveRecon.sh
+++ Recon/Subdomain/ActiveRecon.sh
@@ -26,5 +26,5 @@ bruteforce_enum() {
 knockpy_enum() {
     echo "******************************** Running Knockpy ********************************"
     knockpy -d "$DOMAIN" --recon --save report --json
-    grep '"domain"' "report/</span>{domain}"*.json | awk -F'"' '{print $4}' | tee "knockpy_subdomain.txt"
+    grep '"domain"' "report/{domain}"*.json | awk -F'"' '{print $4}' | tee "knockpy_subdomain.txt"
 }
